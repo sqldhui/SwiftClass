d60cf4b25cbd20cf99b45a7446511dee7a61edc1
sqldhui <lh99998888@163.com>
2015-06-15 18:14:20 +0800

    20150615_1

---
+++ SwiftClassP2C4/SwiftClassP2C4/ListViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ListViewController.swift
+//  SwiftClassP2C4
+//
+//  Created by apple on 15/6/15.
+//  Copyright (c) 2015年 lh. All rights reserved.
+//
+
+import UIKit
+
+class ListViewController: UIViewController , UITableViewDataSource, UITableViewDelegate
+{
+    var items: [String] = ["1", "2"]
+    let cellIdentifier: String = "cellIdentifier"
+    
+    override func viewDidLoad()
+    {
+        var listTabelView: UITableView = UITableView(frame: CGRectMake(0, 20, self.view.frame.size.width, self.view.frame.size.height - 20), style: UITableViewStyle.Grouped)
+        listTabelView.delegate = self
+        listTabelView.dataSource = self
+        self.view.addSubview(listTabelView)
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int
+    {
+        return 1
+    }
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return items.count
+    }
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
+    {
+        var cell: UITableViewCell? = tableView.dequeueReusableCellWithIdentifier(cellIdentifier) as? UITableViewCell
+        if cell == nil
+        {
+            cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: cellIdentifier)
+            cell!.textLabel!.font = UIFont.systemFontOfSize(14)
+            cell!.selectionStyle = .Gray
+            cell!.accessoryType = .DisclosureIndicator
+        }
+        return cell!
+    }
+}
--- SwiftClassP2C4/SwiftClassP2C4/ViewController.swift
+++ SwiftClassP2C4/SwiftClassP2C4/ViewController.swift
@@ -21,6 +21,39 @@ class ViewController: UIViewController {
         
         var viewBounds: CGRect = UIScreen.mainScreen().applicationFrame
         println((viewBounds))
+        
+        var nameLabel: UILabel  = UILabel(frame: CGRectMake(20, 100, 130, 60));
+        nameLabel.backgroundColor = UIColor.redColor()
+        self.view.addSubview(nameLabel)
+        nameLabel.text = "这是一个label"
+        nameLabel.font = UIFont.systemFontOfSize(18)
+        nameLabel.textColor = UIColor.blueColor()
+        nameLabel.shadowColor = UIColor.yellowColor()
+        nameLabel.shadowOffset = CGSizeMake(2, 1)
+        nameLabel.textAlignment = .Center
+        nameLabel.baselineAdjustment = .AlignBaselines
+        
+        var comLabel: UILabel  = UILabel(frame: CGRectMake(160, 100, 130, 60));
+        comLabel.backgroundColor = UIColor.redColor()
+        self.view.addSubview(comLabel)
+        comLabel.text = "这是一个label"
+        comLabel.font = UIFont.systemFontOfSize(18)
+        comLabel.textColor = UIColor.blueColor()
+        comLabel.shadowColor = UIColor.yellowColor()
+        comLabel.shadowOffset = CGSizeMake(2, 1)
+        comLabel.textAlignment = .Center
+        comLabel.baselineAdjustment = .AlignCenters
+        
+        var stepperView: UIStepper = UIStepper(frame: CGRectMake(200, 200, 80, 40))
+        self.view.addSubview(stepperView)
+        println("min: \(stepperView.minimumValue); max: \(stepperView.maximumValue); current: \(stepperView.value)")
+        
+        var progressView: UIProgressView = UIProgressView(frame: CGRectMake(20, 260, 200, 20))
+        self.view.addSubview(progressView)
+        progressView.progress = 0.5
+        progressView.progressViewStyle = .Bar
+        progressView.progressImage = UIImage(named: "track")
+        progressView.trackImage = UIImage(named: "bar")
     }
 
     override func didReceiveMemoryWarning() {
